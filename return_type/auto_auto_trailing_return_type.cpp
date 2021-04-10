484c040c875c2bc9ff952f852a0c9b156a3f4e07
Olcay ATABEY <64277413+olcayatabey@users.noreply.github.com>
2021-04-10 18:37:25 +0300

    Create auto_auto_trailing_return_type.cpp

---
+++ return_type/auto_auto_trailing_return_type.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<initializer_list>
+
+template<typename T>
+auto get_value(T type)->decltype(type)//  decltype(expr)
+{
+	auto value = 0.0;
+	switch (type)
+	{
+	case 1:
+		value = 100.00;
+		break;
+	case 2:
+		value = 50.55;
+		break;
+	case 3:
+		value = 42;
+		break;
+	default:
+		break;
+	}
+	return value;
+}
+
+int main()
+{
+	std::initializer_list<int> ilist{ 1, 2, 3, 4 };
+	for (auto& val : ilist)//range based for loop
+	{
+		std::cout << "value :" << get_value(val) << "\n";
+	}
+}
